c30f8fb5e0ad17cbb67adce9570e0351a33ec365
dhekane2 <dhekane2@illinois.edu>
2026-03-09 11:20:33 -0500

    init project

---
+++ firewall-engine/include/tcp/single-threaded-tcp.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int run_single_threaded_server();
---
+++ firewall-engine/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "tcp/single-threaded-tcp.h"
+
+using namespace std;
+
+int main() {
+	
+	run_single_threaded_server();
+	return 0;
+}
